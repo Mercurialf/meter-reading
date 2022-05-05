a578be81f31433feda9e5c0d42158e437bebf1d7
Alexander Antonenko <mercurialf3@gmail.com>
2022-05-05 20:45:32 +0300

    Improved string formatting

--- Main.java
+++ Main.java
@@ -1,21 +1,11 @@
 public class Main {
-    // Функция проверки корректности суммы к оплате.
-    public static boolean totalScore (double b, double p, double s, double a,
-                                      double r, double c, double f, double t) {
-        if (r > 0) {
-            return (b + s + a + r + c + f) - p == t;
-        } else if (r < 0) {
-            return ((b + s + a + c + f - r) - p == t);
-        }
-
-        return false;
-    }
-
     public static class ScoresList {
         // Добавить функцию расчета суммы необходимой оплаты с помощью тарифа.
+        String monthOfReceipt;
         double beginningOfThePeriod, paid, subscriptionServices, accrued,
                 recalculation, compensation, penaltyCosts, toBePaid;
-        public ScoresList (double[] payslipList) {
+        public ScoresList (String month, double[] payslipList) {
+            this.monthOfReceipt = month;
             this.beginningOfThePeriod = payslipList[0];
             this.paid = payslipList[1];
             this.subscriptionServices = payslipList[2];
@@ -26,28 +16,41 @@ public class Main {
             this.toBePaid = payslipList[7];
         }
 
-        double tariff2022 = 250.00;
-        public void tariffCalculation(int oldScores, int newScores) {
-            double result = newScores - oldScores;
-            if (result < tariff2022) {
-                System.out.print("Tariff result: " + (result * 1.44));
-            } else if (result > tariff2022) {
+        public void tariffCalculation(int oldIndication, int newIndication) {
+            String repeated = repeatString("-", 60);
+            String correctTariffFormat = "%s%n%-40s%20.2f%n%s%n";
+            double minimumTariff = 250.00;
+            double result = newIndication - oldIndication;
+
+            if (result < minimumTariff) {
+                //System.out.print("Tariff result: " + (result * 1.44));
+                System.out.printf(correctTariffFormat, repeated, "Tariff result: " , (result * 1.44), repeated);
+            } else if (result > minimumTariff) {
                 double firstTariff= 250.0;
                 result -= 250.00;
-                System.out.print("Tariff result: " + ((firstTariff * 1.44) + (result * 1.68)));
+                //System.out.print("Tariff result: " + ((firstTariff * 1.44) + (result * 1.68)));
+                System.out.printf(correctTariffFormat, repeated, "Tariff result: ",
+                        ((firstTariff * 1.44) + (result * 1.68)), repeated);
             }
         }
 
         public void totalScoreCorrectTest() {
+            String repeated = repeatString("-", 60);
+            String correctTestFormat = "%s%n%-40s%20.2f%n%-40s%20.2f%n%-40s%n";
             double result;
+
             if (this.recalculation >= 0) {
                 result = (beginningOfThePeriod + subscriptionServices + accrued + recalculation +
                                 compensation + penaltyCosts) - paid;
-                System.out.println("In total you have to pay: " + result + "\\" + toBePaid);
+                //System.out.println("In total you have to pay: " + result + "\\" + toBePaid);
+                System.out.printf(correctTestFormat, repeated, "In total you have to pay: ",
+                        result, "On the receipt: ", toBePaid, repeated);
             } else if (this.recalculation <= 0) {
                 result = (beginningOfThePeriod + subscriptionServices + accrued + compensation +
                         penaltyCosts - recalculation) - paid;
-                System.out.println("In total you have to pay: " + result + "\\" + toBePaid);
+                //System.out.println("In total you have to pay: " + result + "\\" + toBePaid);
+                System.out.printf(correctTestFormat, repeated, "In total you have to pay: ",
+                        result, "On the receipt: ", toBePaid, repeated);
             }
         }
 
@@ -56,11 +59,12 @@ public class Main {
                                     "7. Penalty\\Legal costs: ", "8. To be paid: "};
         public void printScores() {
             String repeated = repeatString("-", 60);
-            String firstFormat = "%s%n%-40s%20.2f%n%s%n";
+            String firstFormat = "%s%n%30s%n%s%n";
             String format = "%-40s%20.2f%n%s%n";
 
             // Попробовать реализовать вывод в консоль с помощью цикла.
-            System.out.printf(firstFormat, repeated, titlesOfScores[0], beginningOfThePeriod, repeated);
+            System.out.printf(firstFormat, repeated, monthOfReceipt, repeated);
+            System.out.printf(format, titlesOfScores[0], beginningOfThePeriod, repeated);
             System.out.printf(format, titlesOfScores[1], paid, repeated);
             System.out.printf(format, titlesOfScores[2], subscriptionServices, repeated);
             System.out.printf(format, titlesOfScores[3], accrued, repeated);
@@ -84,9 +88,10 @@ public class Main {
 
     public static void main(String[] args) {
         double[] payslip = {980.54, 200.00, 0.0, 191.52, 31.68, 0.0, 0.0, 1003.74};
-        ScoresList water = new ScoresList(payslip);
+        ScoresList water = new ScoresList( "March 2022",payslip);
+
         water.printScores();
         water.totalScoreCorrectTest();
-        water.tariffCalculation(10, 888);
+        water.tariffCalculation(260, 320);
     }
 }
