0694226903f1681767f495d5cdcd45f8c9c9d14c
Alexander Antonenko <mercurialf3@gmail.com>
2022-05-13 01:16:54 +0300

    New String array

--- Config.java
+++ Config.java
@@ -2,4 +2,8 @@ public class Config {
     static String[] titlesOfScoresText = {"1. At the beginning of the period: ", "2. Paid: ", "3. Subscriber services: ",
             "4. Accrued: ", "5. Recalculation: ", "6. Compensation: ",
             "7. Penalty\\Legal costs: ", "8. To be paid: "};
+
+    static String[] mainMenuOptions = {"Make a statement", "Show readings", "Checking the payment amount",
+            "Calculation of indications according to the tariff", "Write receipt to file",
+            "Read receipt from file"};
 }
