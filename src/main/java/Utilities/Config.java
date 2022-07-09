53c912e0fbc79ac40850308938e12e95161b0bde
Alexander Antonenko <mercurialf3@gmail.com>
2022-07-09 16:57:01 +0300

    Added JMenuBar and size window

--- src/main/java/Utilities/Config.java
+++ src/main/java/Utilities/Config.java
@@ -2,6 +2,6 @@ package Utilities;
 
 public class Config {
 
-    public static final int SCREEN_WIDTH = 0;
-    public static final int SCREEN_HEIGHT = 0;
+    public static final int SCREEN_WIDTH = 700;
+    public static final int SCREEN_HEIGHT = 520;
 }
