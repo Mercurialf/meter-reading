e200ac9071c9435c5df886fce3c2b00f20145c42
Alexander Antonenko <mercurialf3@gmail.com>
2022-07-17 16:48:58 +0300

    Creating main Config class

--- src/main/java/Utilities/Config.java
+++ src/main/java/Utilities/Config.java
@@ -1,7 +1,18 @@
 package Utilities;
+import java.awt.*;
 
 public class Config {
 
     public static final int SCREEN_WIDTH = 700;
     public static final int SCREEN_HEIGHT = 520;
+
+    public static final Font standardFont = new Font("Ink Free", Font.PLAIN, 16);
+    public static final Font titleFont = new Font("Ink Free", Font.BOLD,18);
+
+    public static final String[] titlesOfScoresText = {"1. At the beginning of the period: ", "2. Paid: ", "3. Subscriber services: ",
+            "4. Accrued: ", "5. Recalculation: ", "6. Compensation: ",
+            "7. Penalty\\Legal costs: ", "8. To be paid: "};
+    public static final String[] mainMenuOptions = {"Make a statement", "Show readings", "Checking payment",
+            "Tariff calculation", "Write receipt to file",
+            "Read receipt from file"};
 }
